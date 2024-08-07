c2ca2568736c0a368d31c4a762498d1ed0502368
bobbycumt <68255033+bobbycumt@users.noreply.github.com>
2024-08-07 18:32:27 +0800

    Update conf.py

--- source/conf.py
+++ source/conf.py
@@ -14,7 +14,8 @@ release = '1.0'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ['recommonmark','sphinx_markdown_tables']
+# extensions = ['recommonmark','sphinx_markdown_tables']
+extensions = ['sphinx_markdown_tables']
 
 templates_path = ['_templates']
 exclude_patterns = []
